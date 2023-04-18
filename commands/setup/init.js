f0f195dd3169fff53a52dc799520b9cf1cd63031
Sebastian Gräßl <mail@bastilian.me>
2023-04-18 22:04:10 +0200

    fix: Restore Default command behaviour
    ... and make eslint happy

--- commands/setup/init.js
+++ commands/setup/init.js
@@ -1,13 +1,22 @@
 import chalk from 'chalk';
 import YAML from 'yaml';
 import { log, readGHuser } from '../../lib/helpers.js';
-import { configHomePath, readConfigFile, saveConfigFile } from '../../lib/configHelpers.js';
+import {
+  configHomePath,
+  readConfigFile,
+  saveConfigFile
+} from '../../lib/configHelpers.js';
 import { asciiHeader } from '../../lib/commandHelpers.js';
-import { selectApplications, inputPrompt, togglePrompt } from '../../lib/promptHelpers.js';
+import {
+  inputPrompt,
+  selectApplications,
+  togglePrompt
+} from '../../lib/promptHelpers.js';
 
 export const help = `
   A Wizard to setup a configuration file (~/.insights-interact.yml)
 `;
+export const defaultCommand = true;
 
 const setConfigPath = () =>
   inputPrompt(
@@ -15,7 +24,9 @@ const setConfigPath = () =>
     configHomePath,
     {
       validate: (value) => (
-        !value.endsWith('.yml') && !value.endsWith('.yaml') ? 'File must a YAML (.yml or .yaml).' : true
+        !value.endsWith('.yml') && !value.endsWith('.yaml')
+          ? 'File must a YAML (.yml or .yaml).'
+          : true
       )
     }
   );
@@ -27,7 +38,8 @@ const confirmedSaveConfigFile = async (config) => {
 
   const saveToHome = await togglePrompt(
     'Want to save the config file to ' + chalk.bold(configHomePath),
-    'Yes', 'No',
+    'Yes',
+    'No',
     { initial: true }
   );
   const savePath = saveToHome ? configHomePath : await setConfigPath();
@@ -38,7 +50,9 @@ const confirmedSaveConfigFile = async (config) => {
 export default async (_cli, _config) => {
   await asciiHeader();
   log.plain('Ahoy!\n');
-  log.plain('This wizard will guide you through creating a config file for setting up Insights frontend applications.\n');
+  log.plain(
+    'This wizard will guide you through creating a config file for setting up Insights frontend applications.\n'
+  );
 
   const currentConfig = await readConfigFile(configHomePath);
 
@@ -48,18 +62,27 @@ export default async (_cli, _config) => {
 
   const basePath = await inputPrompt(
     'Where should the applications be set up?',
-    (currentConfig.basePath || process.env.HOME),
+    currentConfig.basePath || process.env.HOME,
     {
-      validate: (value) => (value === process.env.HOME ? 'Path can not be the home directory' : true)
+      validate: (
+        value
+      ) => (value === process.env.HOME
+        ? 'Path can not be the home directory'
+        : true)
     }
   );
 
   const githubUser = await inputPrompt(
     'What is your GitHub username?',
-    (currentConfig.githubUser || readGHuser())
+    currentConfig.githubUser || readGHuser()
   );
 
-  await confirmedSaveConfigFile({ ...currentConfig, apps, basePath, githubUser });
+  await confirmedSaveConfigFile({
+    ...currentConfig,
+    apps,
+    basePath,
+    githubUser
+  });
 
   log.success('\nThe configuration is now finished.\n');
   log.plain('You can run the setup for the applications with:\n');
